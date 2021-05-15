13ae0432bb93bed0d9c637d308cea53c4d7185f3
ankit <sindeadly25@gmail.com>
2021-05-15 18:42:49 +0530

    same

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,23 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Square from './Square';
 
 const Board = () => {
+  const [board, setBoard] = Array(9).fill(null);
   return (
     <div className="board">
       <div className="board-row">
-        <Square value={0} />
-        <Square value={1} />
-        <Square value={2} />
+        <Square value={board[0]} />
+        <Square value={board[1]} />
+        <Square value={board[2]} />
       </div>
       <div className="board-row">
-        <Square value={3} />
-        <Square value={4} />
-        <Square value={5} />
+        <Square value={board[3]} />
+        <Square value={board[4]} />
+        <Square value={board[5]} />
       </div>
       <div className="board-row">
-        <Square value={6} />
-        <Square value={7} />
-        <Square value={8} />
+        <Square value={board[6]} />
+        <Square value={board[7]} />
+        <Square value={board[8]} />
       </div>
     </div>
   );
