5e525d3478393f6682a1e3085bf1ade9c7e1adca
ankit <sindeadly25@gmail.com>
2021-05-15 18:53:05 +0530

    logic

--- src/components/Board.js
+++ src/components/Board.js
@@ -2,23 +2,52 @@ import React, { useState } from 'react';
 import Square from './Square';
 
 const Board = () => {
-  const [board, setBoard] = Array(9).fill(null);
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [isXNext, setIsXNext] = useState(false);
+
+  const handleSquareClick = position => {
+    if (board[position]) {
+      return;
+    }
+
+    setBoard(prev => {
+      return prev.map((square, pos) => {
+        if (pos === position) {
+          return isXNext ? 'X' : 'O';
+        }
+
+        return square;
+      });
+    });
+
+    setIsXNext(prev => !prev);
+  };
+
+  const renderSquare = position => {
+    return (
+      <Square
+        value={board[position]}
+        onClick={() => handleSquareClick(position)}
+      />
+    );
+  };
+
   return (
     <div className="board">
       <div className="board-row">
-        <Square value={board[0]} />
-        <Square value={board[1]} />
-        <Square value={board[2]} />
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
       </div>
       <div className="board-row">
-        <Square value={board[3]} />
-        <Square value={board[4]} />
-        <Square value={board[5]} />
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
       </div>
       <div className="board-row">
-        <Square value={board[6]} />
-        <Square value={board[7]} />
-        <Square value={board[8]} />
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
       </div>
     </div>
   );
